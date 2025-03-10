03434f87336d4a0dbe624af5e37c8e091b16252c
SpoonKa <spoon1998spoon@gmail.com>
2025-03-10 16:10:14 +0100

    Kalkulator
    1

--- Leson C# git/Leson C# git/Program.cs
+++ Leson C# git/Leson C# git/Program.cs
@@ -1,27 +1,23 @@
 ﻿
-<<<<<<< HEAD
-int a = 4531;
 
-var a1 = a / 1000;
-var a2 = (a - (a1*1000)) / 100;
-var a3 = (a - (a1 * 1000) - (a2*100)) / 10;
-var a4 = (a - (a1 * 1000) - (a2 * 100)- (a3*10));
+using System.ComponentModel.Design;
+
+float a1;
+float a2;
+string s;
 
 
-Console.WriteLine(a1);
-Console.WriteLine(a2);
-Console.WriteLine(a3);
-Console.WriteLine(a4);
-Console.WriteLine(a);
-
-=======
-Console.WriteLine("" +
-            "$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$" +
-    "\r\n\r\n$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______" +
-    "\r\n\r\n$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______" +
-    "\r\n\r\n$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__" +
-    "\r\n\r\n$$$______$$$_______$$$_____$$$_$$$____$$$_______" +
-    "\r\n\r\n$$$_______$$$_____$$$______$$$_$$$____$$$_______" +
-    "\r\n\r\n$$$$$$$$$___$$$$$$$_________$$$$$_____$$$$$$$$$$");
->>>>>>> origin/main
-Console.ReadLine();
+Console.WriteLine("Fifst Value");
+a1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Second Value");
+a2 = float.Parse(Console.ReadLine());
+Console.WriteLine("char");
+s = Console.ReadLine();
+if (s == "+")
+    Console.WriteLine(a1 + a2);
+else if (s == "-")
+    Console.WriteLine(a1 - a2);
+else if (s == "*") 
+Console.WriteLine(a1 * a2);
+        else if (s == "/") 
+Console.WriteLine(a1 / a2);
